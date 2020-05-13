5e19ebab0c8d5eba2676d3b5baced1095a765a8d
Juan Pablo <jp.cano@uniandes.edu.co>
2020-05-13 13:04:30 -0500

    Euler Backward

--- mundo/functions.py
+++ mundo/functions.py
@@ -2,9 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(t, y):
-    return (0.49 - ((0.00245 * np.exp(0.49 * t)) / (0.49 + 0.005 * (np.exp(0.49 * t))))) * y
+    return (0.49 - ((0.00245 * np.exp(0.49 * t)) / (0.49 + 0.005 * (np.exp(0.49 * t) - 1)))) * y
 
-def euler_forward(func, h = 0.01, y_0 = 0.01, t_0 = 0., t_f = 30.):
+def f1_euler_back(t, y, h):
+    return y / (1 - h * (0.49 - ((0.00245 * np.exp(0.49 * t)) / (0.49 + 0.005 * (np.exp(0.49 * t) - 1)))))
+
+def euler_forward(func, h=0.01, y_0=0.01, t_0=0., t_f=30.):
     """
     Calcula la solución de una ecuación diferencial a través
     del método de euler hacia adelante
@@ -21,7 +24,15 @@ def euler_forward(func, h = 0.01, y_0 = 0.01, t_0 = 0., t_f = 30.):
     for i in range(1, len(T)):
         # Vamos estimando el valor de Y_i
         y_euler[i] = y_euler[i - 1] + h * func(T[i - 1], y_euler[i - 1])
+    return T, y_euler
 
+def euler_backward(func, h=0.01, y_0=0.01, t_0=0., t_f=30.):
+    T = np.arange(t_0, t_f + h, h)
+    y_euler = np.zeros(len(T))
+    y_euler[0] = y_0
+
+    for i in range(1, len(T)):
+        y_euler[i] = f1_euler_back(T[i], y_euler[i-1], h)
     return T, y_euler
 
 def y_analitc(t):
@@ -39,19 +50,3 @@ def y_analitc(t):
     expr1 = ((a * N-b) * i_0 * np.exp((a*N - b)*t))
     expr2 = ((a * N-b) + a * i_0 * (np.exp((a*N - b)*t)-1))
     return expr1 / expr2
-
-h_values = np.array([5., 1., 0.5, 0.1, 0.05])
-T_euler = np.arange(0, 30, 0.1)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-for index in h_values:
-    T, y = euler_forward(f, h=index)
-    ax.plot(T, y, linestyle="--", label=f"Euler h: {index}")
-ax.plot(T_euler, y_analitc(T_euler), linestyle="--", label="Analitica")
-ax.set_xlabel("t", fontsize=15)
-ax.set_ylabel("Y(t)", fontsize=15)
-ax.grid(linestyle="--")
-ax.legend(loc="best")
-plt.show()
