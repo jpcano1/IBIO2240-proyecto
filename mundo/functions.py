e0963152c6b1f3de76be2c635aff506ae67a881d
laura chadid cohen <lv.chadid@uniandes.edu.co>
2020-05-17 15:27:11 -0500

    Euler modificado

--- mundo/functions.py
+++ mundo/functions.py
@@ -7,6 +7,9 @@ def f(t, y):
 def f1_euler_back(t, y, h):
     return y / (1 - h * (0.49 - ((0.00245 * np.exp(0.49 * t)) / (0.49 + 0.005 * (np.exp(0.49 * t) - 1)))))
 
+def f1_euler_mod(t, y):
+    return (1 - (h / 2.0) * (0.49 - ((0.00245 * np.exp(0.49 * t)) / (0.49 + 0.005 * (np.exp(0.49 * t) - 1)))))
+
 def euler_forward(func, h=0.01, y_0=0.01, t_0=0., t_f=30.):
     """
     Calcula la solución de una ecuación diferencial a través
@@ -34,6 +37,15 @@ def euler_backward(func, h=0.01, y_0=0.01, t_0=0., t_f=30.):
     for i in range(1, len(T)):
         y_euler[i] = f1_euler_back(T[i], y_euler[i-1], h)
     return T, y_euler
+
+def euler_modified(func, h=0.01, y_0=0.01, t_0=0., t_f=30.)
+    T = np.arange(t_0, t_f + h, h)
+    y_euler = np.zeros(len(T))
+    y_euler[0] = y_0
+
+    for i in range(1, len(T)):
+        y_euler[i] = (y_euler[i - 1] + (h / 2.0) * f1(T[i - 1], y_euler[i - 1])) / \ f1_euler_mod(T[i], h)
+        return T, y_euler
 
 def y_analitc(t):
     # Tasa de infeccion
