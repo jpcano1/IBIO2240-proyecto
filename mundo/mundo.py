20a15bb3f24bc3f7ae1cf89ce01881282948da95
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-05-27 12:07:27 -0500

    Ecuación Z

--- mundo/mundo.py
+++ mundo/mundo.py
@@ -12,6 +12,18 @@ def y_dot(x, y, w):
     alpha = 1 - np.sqrt(x**2 + y**2)
     return alpha*y + w*x
 
-def z_dot(x, y):
-    theta = np.arctan2(x, y)
-    return
+def z_dot(x, y, z, *args):
+    theta = np.arctan2(y, x)
+    result = 0
+    for arg in args:
+        a_i = parametros.loc["a_i", arg]; b_i = parametros.loc["b_i", arg]
+        theta_i = parametros.loc["Theta_i", arg]; t = parametros.loc["Time", arg]
+        delta_theta_i = (theta - theta_i) % 2 * np.pi
+        result += a_i * delta_theta_i * np.exp(-(delta_theta_i**2 / (2 * b_i**2))) \
+                  - (z - z_0(t))
+    return result
+
+def z_0(t):
+    """Falta meter la frecuencia"""
+    A = 0.15
+    return A * np.sin(2 * np.pi * t)
