30b26cb3849aa16a0c4f56c015fc2bc09a68676e
casalazara <ca.salazara@uniandes.edu.co>
2020-06-02 15:49:50 -0500

    Update panel_puntos.py

--- interfaz/panel_puntos.py
+++ interfaz/panel_puntos.py
@@ -19,54 +19,64 @@ class PanelPuntos(Frame):
         Label(self, text="P",background='#212946',foreground='#08F7FE').grid(row=0, column=1)
 
         self.p_a = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.p_a.grid(row=1, column=1)
 
         self.p_b = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.p_b.grid(row=2, column=1)
 
         Label(self, text="Q",background='#212946',foreground='#08F7FE').grid(row=0, column=2)
 
         self.q_a = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.q_a.grid(row=1, column=2)
 
         self.q_b = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.q_b.grid(row=2, column=2)
 
 
         Label(self, text="R",background='#212946',foreground='#08F7FE').grid(row=0, column=3)
 
         self.r_a = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.r_a.grid(row=1, column=3)
 
         self.r_b = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.r_b.grid(row=2, column=3)
 
         Label(self, text="S",background='#212946',foreground='#08F7FE').grid(row=0, column=4)
 
         self.s_a = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.s_a.grid(row=1, column=4)
 
         self.s_b = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.s_b.grid(row=2, column=4)
 
         Label(self, text="T",background='#212946',foreground='#08F7FE').grid(row=0, column=5)
 
         self.t_a = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.t_a.grid(row=1, column=5)
 
         self.t_b = Entry(master=self, background='#212946', foreground='#dad0c0',
-                                      insertbackground='#dad0c0')
+                                      insertbackground='#dad0c0', width=3, highlightbackground="#F5D300",
+                                    highlightcolor="#F5D300", highlightthickness=1, relief="solid")
         self.t_b.grid(row=2, column=5)
 
 
-        Label(self, text="a_i",background='#212946', foreground='#FE53BB').grid(row=1, column=0)
-        Label(self, text="b_i",background='#212946', foreground='#FE53BB').grid(row=2, column=0)
+        Label(self, text="a_i",background='#212946', foreground='#FE53BB', font=('calibri', 12, 'bold')).grid(row=1, column=0)
+        Label(self, text="b_i",background='#212946', foreground='#FE53BB', font=('calibri', 12, 'bold')).grid(row=2, column=0)
